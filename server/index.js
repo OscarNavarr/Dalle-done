4628ebb228f673dabbbf33df5b578bcd5ce8194a
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-02-09 19:03:50 +0100

    Conection with cloudinary, and we can see les images on CreatePost page

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 import express from 'express';
-import *  as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-// MONGODB
 import connectDB from './mongodb/connect.js';
-
-//Routes
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
@@ -13,28 +10,24 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-    res.send('Hello from DALL-E!');
-})
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
 const startServer = async () => {
-    
-    try {
-        
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
-
-    } catch (error) {
-        
-        console.log(error);
-
-    }
-
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
